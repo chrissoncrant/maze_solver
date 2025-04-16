e9a2bd5fb3ee7d6ae0c9f81063a890938c3009b6
chrissoncrant <christopher.soncrant@gmail.com>
2025-04-16 13:26:45 -0700

    added Line and Point classes

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from window import Window
+from window import Window, Point, Line
 
 def main():
     print("Main Function Running...")
 
     win = Window(800, 600)
+    
+    line_1 = Line(Point(0, 0), Point(100, 100))
+
+    win.draw_line(line_1, "black")
 
     win.wait_for_close()
 
